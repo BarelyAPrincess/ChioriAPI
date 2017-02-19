b5cc1c0f0fc95440a4ad475beafa122563d59f00
Chiori Greene <chiorigreene@gmail.com>
2017-02-19 01:19:00 -0600

    New FileWatcher class, precursor to live Configuration load and saving

--- src/main/java/com/chiorichan/io/FileWatcher.java
+++ src/main/java/com/chiorichan/io/FileWatcher.java
@@ -1,8 +1,79 @@
 package com.chiorichan.io;
 
-/**
- * Created by chiori on 2/18/17.
- */
-public class FileWatcher
+import com.chiorichan.tasks.Task;
+import com.chiorichan.tasks.TaskManager;
+import com.chiorichan.tasks.TaskRegistrar;
+import com.chiorichan.tasks.Ticks;
+import com.chiorichan.tasks.Timings;
+
+import java.io.File;
+
+public abstract class FileWatcher implements TaskRegistrar, Runnable
 {
+	protected final File fileToWatch;
+	private long lastModified = 0;
+	private long cycleCoolDown = 1;
+	private long lastCheck;
+	private Task task;
+
+	public FileWatcher( File fileToWatch )
+	{
+		this.fileToWatch = fileToWatch;
+
+		task = TaskManager.instance().runTask( this, this );
+	}
+
+	@Override
+	public final void run()
+	{
+		boolean changesDetected = false;
+		lastCheck = Timings.epoch();
+
+		if ( fileToWatch.exists() )
+		{
+			long newLastModified = fileToWatch.lastModified();
+			changesDetected = newLastModified > lastModified;
+
+			if ( changesDetected )
+			{
+				lastModified = newLastModified;
+				cycleCoolDown = 1;
+				readChanges();
+			}
+		}
+
+		if ( !changesDetected && Ticks.SECOND_5 * cycleCoolDown < Ticks.MINUTE_15 )
+			cycleCoolDown++;
+
+		task = TaskManager.instance().runTaskLater( this, Ticks.SECOND_5 * cycleCoolDown, this );
+	}
+
+	/**
+	 * Resets the cool down period and runs the task sooner
+	 */
+	public final void reviveTask()
+	{
+		if ( cycleCoolDown == 1 )
+			return;
+
+		cycleCoolDown = 1;
+		task.cancel();
+
+		task = TaskManager.instance().runTaskLater( this, Timings.epoch() - lastCheck >= 5 ? 0L : Ticks.SECOND_5 - ( Ticks.SECOND * ( Timings.epoch() - lastCheck ) ), this );
+	}
+
+	public abstract void readChanges();
+
+
+	@Override
+	public final boolean isEnabled()
+	{
+		return true;
+	}
+
+	@Override
+	public String getName()
+	{
+		return "FileWatcher";
+	}
 }
